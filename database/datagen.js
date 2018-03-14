58d33ff3245304bdb86f426fd14f9f775d4f8cf3
Zachary <zinouye@gmail.com>
2018-03-14 16:54:27 -0700

    created dropDB file and installed faker

--- database/datagen.js
+++ database/datagen.js
@@ -1,2 +1,15 @@
 const faker = require('faker');
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/bookings');
+
+const genOneData = (id) => {
+  let booking = '';
+  return booking;
+};
+
+const genAllData = (quantity) => {
+
+};
+
+exports.genAllData = genAllData;
--- database/index.js
+++ database/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/bookings');
 const data = require('../fakeData.js');
 
-let bookingsSchema = mongoose.Schema({
+const bookingsSchema = mongoose.Schema({
   id: Number,
   unavailableDates: Array,
   rating: Number,
@@ -11,11 +12,11 @@ let bookingsSchema = mongoose.Schema({
   cost: Number,
   minStay: Number,
   maxStay: Number,
-  childrenAllowed: Boolean
+  childrenAllowed: Boolean,
 });
 
-let Booking = mongoose.model('Booking', bookingsSchema);
-const save = (singleBooking, callback) => {
+const Booking = mongoose.model('Booking', bookingsSchema);
+const save = (singleBooking) => {
   const newBooking = new Booking({
     id: singleBooking.id,
     unavailableDates: singleBooking.unavailable_dates,
@@ -25,32 +26,32 @@ const save = (singleBooking, callback) => {
     cost: singleBooking.cost,
     minStay: singleBooking.min_stay,
     maxStay: singleBooking.max_stay,
-    childrenAllowed: singleBooking.children_allowed
+    childrenAllowed: singleBooking.children_allowed,
   });
-  newBooking.save(function (err, fluffy) {
+  newBooking.save((err) => {
     if (err) {
       console.error(err);
     } else {
       console.log('successfully saved');
     }
-  })
+  });
 };
 
 const other = () => {
-  for (var i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i += 1) {
     save(data[i]);
   }
-}
+};
 
 
 const find = (id, callback) => {
-  Booking.find({id : id}, function (err, item){
+  Booking.find({ id : id }, (err, item) => {
     callback(item);
-  })
-}
+  });
+};
 
 module.exports.other = other;
 module.exports.find = find;
 module.exports.db = mongoose;
-module.exports.booking = Booking;
+module.exports.Booking = Booking;
 module.exports.save = save;
