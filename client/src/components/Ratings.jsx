dca8cf607de5c7ca918772a0d3a04d43aadca00c
Zachary <zinouye@gmail.com>
2018-03-17 13:41:40 -0700

    modularize Ratings css

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -4,28 +4,7 @@ import FaStarHalf from 'react-icons/lib/fa/star-half';
 import FaStarHalfEmpty from 'react-icons/lib/fa/star-half-empty';
 import FaStar from 'react-icons/lib/fa/star';
 import styled from 'styled-components';
-
- const RatingsMain = styled.div`
-   height: 20px;
-   width: 100px;
-   display: flex;
-   align-items: flex-end;
-   justify-content: flex-start;
-   font-family: 'Quicksand', sans-serif;
-
- `;
- const Stars = styled.div`
-  font-size: 13px;
-  color: #55b0b2;
-  align-self: center;
-`;
-const RatingsAmount = styled.div`
-  font-size: 15px;
-  align-items: flex-end;
-  padding-left: 3px;
-  font-family: 'Quicksand', sans-serif;
-
-`;
+import { RatingsMain, Stars, RatingsAmount } from './Ratings.css';
 
 class Ratings extends React.Component {
   constructor(props) {
@@ -53,14 +32,14 @@ class Ratings extends React.Component {
   render (props) {
 
     return (
-      <RatingsMain>
-        <Stars>
+      <div className={RatingsMain}>
+        <div className={Stars}>
           {this.state.starsArr.map(item => (
             item
           ))}
-        </Stars>
-        <RatingsAmount>{this.props.ratingAmount}</RatingsAmount>
-      </RatingsMain>
+        </div>
+        <div className={RatingsAmount}>{this.props.ratingAmount}</div>
+      </div>
     )
   }
 }
