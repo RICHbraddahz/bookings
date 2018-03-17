66108b47bd2c1f6ea52cdd8da1a470de9b9beffe
Zachary <zinouye@gmail.com>
2018-03-17 12:34:51 -0700

    modularized Guests.jsx with css-modules

--- client/src/components/Guests.jsx
+++ client/src/components/Guests.jsx
@@ -1,85 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Main, Guest, SelectGuest, GuestChoices, MainGuestSelection, Guest }
+import { Main, Guest, SelectGuest, GuestChoices, MainGuestSelection, GuestMax, Choice, UpCount } from './Guests.css';
 
-
-const Main = styled.div`
-  width: 100px;
-  height: 100px;
-  display: flex;
-  flex-direction: column;
-  font-family: 'Quicksand', sans-serif;
-  text-align: left;
-  direction: rtl
-`;
-const Guest = styled.div`
-  font-size: 12px;
-  color: grey;
-`;
-const SelectGuests = styled.button`
-  height: 42px;
-  width: 131px;
-  background-color: white;
-  border-color: #cccccc;
-  border-radius: 2px;
-
-`;
-const GuestChoices = styled.div`
-  height: 306px;
-  width: 280px;
-  align: right;
-  margin-left:auto; margin-right:0;
-
-  align-self: flex-end;
-  float: right;
-  direction: ltr
-`;
-const MainGuestSelection = styled.div`
-  height: 306px;
-  width: 280px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: space-around;
-  background-color: white;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  border-radius: 5px;
-  z-index: 10;
-  margin-top: 35px;
-`;
-const GuestMax = styled.div`
-  height: 65px;
-  width: 275px;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  margin-left: 5px;
-`;
-const Choice = styled.div `
-  height: 65px;
-  width: 280px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
-const DisableChoice = Choice.extend`
-  color: grey;
-  border-color: grey;
-`;
+// const DisableChoice = Choice.extend`
+//   color: grey;
+//   border-color: grey;
+// `;
 
  const UpCountMain = styled.div`
    width: 80px;
    height: 30px;
    font-size: 20px;
  `;
- const UpCount = styled.button`
-  width: 40px;
-  height: 40px;
-  border-color: #55b0b2;
-  color: #55b0b2;
-  border-radius: 50%;
-  font-size: 20px;
-`;
 const Close = styled.button `
   width: 50px;
   align-self: flex-end;
@@ -89,31 +21,29 @@ const Close = styled.button `
     text-decoration: underline;
   }
 `;
-const Test = styled.div`
-width: 40px;
-  height: 40px;
-`;
-const DisableCount = UpCount.extend`
-  color: grey;
-  border-color: grey;
-`;
+
+// const DisableCount = UpCount.extend`
+//   color: grey;
+//   border-color: grey;
+// `;
 
 let GuestSelection = (props) => {
     return (
-     <MainGuestSelection>
-      <Choice> <UpCountMain> Adult</UpCountMain> <UpCount onClick={ () => (props.handleAddGuest('adult', 'subtract'))}>-</UpCount> {props.adultAmount}{props.maxHit === true ? <DisableCount> +</DisableCount> : <UpCount onClick={ () => (props.handleAddGuest('adult', 'add'))}>+</UpCount>} </Choice>
+     <div className={MainGuestSelection}>
+      <div className={Choice}> <UpCountMain> Adult</UpCountMain> <button className={UpCount} onClick={ () => (props.handleAddGuest('adult', 'subtract'))}>-</button> {props.adultAmount}{props.maxHit === true ? <DisableCount> +</DisableCount> : <button className={UpCount} onClick={ () => (props.handleAddGuest('adult', 'add'))}>+</button>} </div>
       {props.childrenAllowed === true
-        ? <Choice>  <UpCountMain>Children </UpCountMain> <UpCount onClick={ () => (props.handleAddGuest('children', 'subtract'))}>-</UpCount> {props.childrenAmount}{props.maxHit === true ? <DisableCount> +</DisableCount> : <UpCount onClick={ () => (props.handleAddGuest('children', 'add'))}>+</UpCount>} </Choice>
-        : <DisableChoice>  <UpCountMain>Children </UpCountMain> <UpCount>-</UpCount> {props.childrenAmount} <UpCount>+</UpCount> </DisableChoice>
+        ? <Choice>  <UpCountMain>Children </UpCountMain> <button className={UpCount} onClick={ () => (props.handleAddGuest('children', 'subtract'))}>-</button> {props.childrenAmount}{props.maxHit === true ? <DisableCount> +</DisableCount> : <button className={UpCount} onClick={ () => (props.handleAddGuest('children', 'add'))}>+</button>} </Choice>
+        : <DisableChoice>  <UpCountMain>Children </UpCountMain> <button className={UpCount}>-</button> {props.childrenAmount} <button className={UpCount}>+</button> </DisableChoice>
       }
       {props.childrenAllowed === true
-        ? <Choice>  <UpCountMain>Infants </UpCountMain> <UpCount onClick={ () => (props.handleAddGuest('infant', 'subtract'))}>-</UpCount> {props.infantAmount} {props.infantMaxHit === true ? <DisableCount>+ </DisableCount> : <UpCount onClick={ () => (props.handleAddGuest('infant', 'add'))}>+</UpCount>} </Choice>
-        : <DisableChoice>  <UpCountMain>Infants </UpCountMain> <UpCount>-</UpCount> {props.infantAmount} <UpCount>+</UpCount> </DisableChoice>
+        ? <Choice>  <UpCountMain>Infants </UpCountMain> <button className={UpCount} onClick={ () => (props.handleAddGuest('infant', 'subtract'))}>-</button> {props.infantAmount} {props.infantMaxHit === true ? <DisableCount>+ </DisableCount> : <button className={UpCount} onClick={ () => (props.handleAddGuest('infant', 'add'))}>+</button>} </Choice>
+        : <DisableChoice>  <UpCountMain>Infants </UpCountMain> <button className={UpCount}>-</button> {props.infantAmount} <button className={UpCount}>+</button> </DisableChoice>
       }
-      <GuestMax> {props.guestMax} guests maximum. Infants don’t count toward the number of guests.</GuestMax>
+      <div className={GuestMax}> {props.guestMax} guests maximum. Infants don’t count toward the number of guests.</div>
       <Close onClick={props.close}> Close</Close>
-     </MainGuestSelection>
+     </div>
     )
+
 
 }
 
@@ -191,16 +121,14 @@ class Guests extends React.Component {
 
   }
   render () {
-
-
     return (
-      <Main>
-        <Guest> Guests </Guest>
-        <SelectGuests onClick={this.toggleShowComponent}>
+      <div className={Main}>
+        <div className={Guest}> Guests </div>
+        <button className={SelectGuest} onClick={this.toggleShowComponent}>
          {this.state.guestAmount} Guest
-        </SelectGuests>
+        </button>
         {this.state.showComponent
-          ? <GuestChoices>
+          ? <div className={GuestChoices}>
               <GuestSelection
                 handleAddGuest={this.handleAddGuest}
                 guestMax={this.props.guest_max}
@@ -212,11 +140,11 @@ class Guests extends React.Component {
                 maxHit={this.state.maxHit}
                 infantMaxHit={this.state.infantMaxHit}
               />
-            </GuestChoices>
+            </div>
           : null
         }
 
-      </Main>
+      </div>
 
 
     )
