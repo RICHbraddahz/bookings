81bc0380b219bf6abcc8f2fd4ae5224d7a313710
Zachary <zinouye@gmail.com>
2018-03-19 09:44:51 -0700

    finished schema

---
+++ database/PostgreSQL/createTableSchema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE similarlistings (
+  id SERIAL PRIMARY KEY,
+  unavailableDates TEXT NOT NULL,
+  type TEXT NOT NULL,
+  rating INT NOT NULL,
+  numberOfRatings INT NOT NULL,
+  guestMax INT NOT NULL,
+  cost INT NOT NULL,
+  minStay INT NOT NULL,
+  maxStay INT NOT NULL,
+  childrenAllowed BOOLEAN NOT NULL
+);
