abcaa7597e8719aa88bfe95fa5bf52cbc2cef6bc
Zachary <zinouye@gmail.com>
2018-03-20 17:25:24 -0700

    wrote scripts to restart pg, drop pg database, create pg database, table, and begin seeding in postgres

--- database/PostgreSQL/dropDB-postgres.sql
+++ database/PostgreSQL/dropDB-postgres.sql
@@ -1,2 +1,9 @@
-select pg_terminate_backend(procpid) from pg_stat_activity where datname='seabnb';
-DROP DATABASE "seabnb";
+REVOKE CONNECT ON DATABASE seabnb FROM PUBLIC, zacharyinouye;
+SELECT 
+    pg_terminate_backend(pid) 
+FROM 
+    pg_stat_activity 
+WHERE 
+    pid <> pg_backend_pid()
+    AND datname = 'database_name';
+DROP DATABASE seabnb;
