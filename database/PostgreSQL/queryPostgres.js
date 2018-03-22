ec0b909605591585a5c1dea5f0d7b7eb8aee22e0
Zachary <zinouye@gmail.com>
2018-03-22 11:21:35 -0700

    logging individual query times

--- database/PostgreSQL/queryPostgres.js
+++ database/PostgreSQL/queryPostgres.js
@@ -6,22 +6,19 @@ const dateMath = require('date-arithmetic');
 const db = pgp('postgres://localhost:5432/seabnb');
 const startId = 0;
 const endId = 10000000;
-const idCount = 20;
+const ids = 10000;
 
 const getAverage = array => (array.reduce((acc, val) => acc + val)) / array.length;
 
 const getBenchmark = async () => {
   const startTime = new Date();
-
   console.log('|******************************************************');
   console.log('| Building benchmark for PostgreSQL indexed by id');
-  console.log(`| Querying ${idCount} ids from ${startId} to ${endId}`);
+  console.log(`| Querying ${ids} ids from ${startId} to ${endId}`);
   console.log(`| Start time: ${startTime}`);
   console.log('|******************************************************');
-
   const readTimes = [];
-
-  for (let i = 0; i < 10000; i += 1) {
+  for (let i = 0; i < ids; i += 1) {
     const readStartTime = new Date();
     const randomNum = Math.floor(Math.random() * 10000000);
     try {
