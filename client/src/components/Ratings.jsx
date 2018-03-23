bf581a724c8243d764801839c777b19fae146569
Zachary <zinouye@gmail.com>
2018-03-23 16:16:00 -0700

    made code more readable

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -3,13 +3,10 @@ import FaStar from 'react-icons/lib/fa/star';
 import { RatingsMain, Stars, RatingsAmount } from './Ratings.css';
 
 const Ratings = ({ stars, ratingAmount }) => {
-  let starsArr = [];
-  for (let i = 0; i < stars; i++) {
-  // note: we add a key prop here to allow react to uniquely identify each
-  // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-    starsArr.push(<FaStar/>);
+  const starsArr = [];
+  for (let i = 0; i < stars; i += 1) {
+    starsArr.push(<FaStar />);
   }
-
   return (
     <div className={RatingsMain}>
       <div className={Stars}>
@@ -17,7 +14,7 @@ const Ratings = ({ stars, ratingAmount }) => {
       </div>
       <div className={RatingsAmount}>{ratingAmount}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Ratings;
