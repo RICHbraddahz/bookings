0ad06c3cd344a73636949265f2052d05123889b5
Zachary <zinouye@gmail.com>
2018-03-26 11:05:08 -0700

    fixed routes

--- client/src/components/Bookings.jsx
+++ client/src/components/Bookings.jsx
@@ -54,7 +54,7 @@ class Bookings extends React.Component {
     const that = this;
     $.ajax({
       type: 'GET',
-      url: `http://localhost:3002/api/bookings/${listing}`,
+      url: `/api/bookings/${listing}`,
       success: (data) => {
         that.setState({ data: data[0].bookings });
       },
